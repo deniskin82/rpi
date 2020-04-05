578022a7a2ee9a0ac9d9168689f2406ee55cb4b3
deniskin82 <denis.corol@gmail.com>
2020-04-05 19:57:37 -0400

    install ntpd

--- master.rb
+++ master.rb
@@ -2,3 +2,4 @@ node[:role] = 'master'
 include_recipe('recipes/coredns/install.rb')
 include_recipe('recipes/motd/limited.rb')
 include_recipe('recipes/coredns/run.rb')
+include_recipe('recipes/ntpd/default.rb')
