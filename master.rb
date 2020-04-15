d462027bf6b265f8300286ed1bfa6fd32504ca2e
deniskin82 <denis.corol@gmail.com>
2020-04-15 17:07:14 -0400

    install monit, moved settings to yaml

--- master.rb
+++ master.rb
@@ -1,4 +1,5 @@
 node[:role] = 'master'
+include_recipe('recipes/monit/install.rb')
 include_recipe('recipes/coredns/install.rb')
 include_recipe('recipes/motd/limited.rb')
 include_recipe('recipes/coredns/run.rb')
---
+++ recipes/monit/install.rb
@@ -0,0 +1,38 @@
+define :install_monit do
+  cwd = File.dirname(__FILE__)
+  package 'facter'
+  linux_arch    = case node[:kernel][:machine]
+  when 'aarch64'
+    'arm64'
+  when 'x86_64'
+    'x64'
+  end
+  monit_version = ENV['monit_VERSION'] || params[:version] || node[:versions][:monit] || '5.26.0'
+  monit_path    = node[:monit][:homedir] || '/opt/monit'
+  monit_url     = "https://mmonit.com/monit/dist/binary/#{monit_version}/monit-#{monit_version}-linux-#{linux_arch}.tar.gz"
+  monit_tar     = '/tmp/monit.tgz'
+  monit_confdir = node[:monit][:confdir] || "#{monit_path}/config"
+  monit_datadir = node[:monit][:datadir] || "#{monit_path}/data"
+
+  directory "#{monit_path}/bin"
+
+  directory "#{monit_confdir}"
+
+  directory "#{monit_datadir}"
+
+  http_request "#{monit_tar}" do
+    url "#{monit_url}"
+    not_if "test -e #{monit_tar}"
+  end
+
+  execute 'extract_monit' do
+    command "tar --strip 1 -C #{monit_path} --wildcards -zxf #{monit_tar} */bin/monit"
+    not_if "test -e #{monit_path}/bin/monit"
+  end
+
+  link '/usr/local/sbin/monit' do
+    to "#{monit_path}/bin/monit"
+  end
+end
+
+install_monit 'monit'
