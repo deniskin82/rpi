8203a574cb16ca238092cf46d1347cefd7789702
deniskin82 <denis.corol@gmail.com>
2020-04-15 21:43:20 -0400

    Update install.rb

--- recipes/docker/install.rb
+++ recipes/docker/install.rb
@@ -7,6 +7,13 @@ when '16.04'
   'xenial'
 end
 
+linux_arch    = case node[:kernel][:machine]
+when 'aarch64'
+  'arm64'
+when 'x86_64'
+  'amd64'
+end
+
 %w(
   apt-transport-https
   ca-certificates
@@ -16,12 +23,6 @@ end
   package pkg
 end
 
-linux_arch    = case node[:kernel][:machine]
-when 'aarch64'
-  'arm64'
-when 'x86_64'
-  'amd64'
-end
 execute "add-apt-repository -y 'deb [arch=#{linux_arch}] https://download.docker.com/linux/ubuntu #{family_name} stable'" do
   not_if 'grep -q docker /etc/apt/sources.list'
   notifies :run, 'execute[apt-get update]', :immediately
