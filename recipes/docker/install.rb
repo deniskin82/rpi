5a6729acc6b716a3e7bbf617e523efeb28761279
deniskin82 <denis.corol@gmail.com>
2020-04-15 21:12:10 -0400

    Create install.rb
    install docker

---
+++ recipes/docker/install.rb
@@ -0,0 +1,20 @@
+linux_arch    = case node[:kernel][:machine]
+when 'aarch64'
+  'arm64'
+when 'x86_64'
+  'amd64'
+end
+execute "add-apt-repository -y 'deb [arch=#{linux_arch}] https://download.docker.com/linux/ubuntu bionic stable'" do
+  not_if 'grep -q docker /etc/apt/sources.list'
+  notifies :run, 'execute[apt-get update]', :immediately
+end
+
+execute 'apt-get update' do
+  action :nothing
+end
+
+%w(
+  docker-ce
+).each do |pkg|
+  package pkg
+end
